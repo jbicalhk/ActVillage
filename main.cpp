54df6865324f0474ad7ea29e9321643849f616cf
João Bicalho <joaobicalho03@gmail.com>
2023-08-26 13:48:37 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <iostream>
+#include <tmxlite/Map.hpp>
+
+using namespace std;
+
+#include "SFMLOrthogonalLayer.hpp"
+
+int main(int argc, char **argv) {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	cout << "1" << endl;
+	tmx::Map map;
+
+	if (map.load("assets/demo.tmx")) {
+
+		MapLayer layerZero(map, 0);
+		MapLayer layerOne(map, 1);
+		MapLayer layerTwo(map, 2);
+		MapLayer layerThree(map, 3);
+
+		sf::Clock globalClock;
+		while (window.isOpen()) {
+			sf::Event event;
+			while (window.pollEvent(event)) {
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			window.clear(sf::Color::Black);
+			window.draw(layerZero);
+			window.draw(layerOne);
+			window.draw(layerTwo);
+			window.draw(layerThree);
+			window.display();
+		}
+
+	}
+	return 0;
+}
